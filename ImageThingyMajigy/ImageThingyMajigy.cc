b104a3f5e55343cfbc8667dfd5fa8795ccd94d00
andrekorol <anrobits@yahoo.com.br>
2018-11-08 21:39:13 -0200

    Remove encoding testing comment

--- ImageThingyMajigy/ImageThingyMajigy.cc
+++ ImageThingyMajigy/ImageThingyMajigy.cc
@@ -10,6 +10,6 @@ int main(int argc, char **argv) {
 	Magick::InitializeMagick(*argv);
 	fs::path dest = "C:/Users/anrob/OneDrive/Imagens/SonyA100";
 	ImageThingyMajigy::Organize(dest);
-	// Test UTF-8 emcoding on GitHub
+
 	return 0;
 }
